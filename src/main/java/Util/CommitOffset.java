b00b9d6f5b470e281ee335a3b05dcf2625834bd9
rishimi <rishi.mi@directi.com>
2015-11-16 01:12:27 +0530

    Partition offsets rollback in case commit is not  working

--- src/main/java/Util/CommitOffset.java
+++ src/main/java/Util/CommitOffset.java
@@ -15,23 +15,38 @@ import java.util.Map;
 public class CommitOffset {
 
 
+    public static void commitPartitionOffsets(String topic, String topicHost, Integer topicPort, String consumerGroup, Map<Integer, Long> partitionOffsets) {
 
-    public static void commitPartitionOffsets(String topic, String topicHost, Integer topicPort , String consumerGroup, Map<Integer, Long> partitionOffsets){
+        Map<Integer, Long> oldPartitionOffsets = getOldOffsets(topic,topicHost,topicPort,consumerGroup,partitionOffsets);
 
         for (Map.Entry<Integer, Long> entry : partitionOffsets.entrySet()) {
-
             TopicAndPartition topicAndPartition = new TopicAndPartition(topic, entry.getKey());
-            OffsetAndMetadata offsetAndMetadata = new OffsetAndMetadata(entry.getValue(),OffsetAndMetadata.NoMetadata(),-1);
-            Map<TopicAndPartition,OffsetAndMetadata> responseInfo = new HashMap<TopicAndPartition, OffsetAndMetadata>();
-            responseInfo.put(topicAndPartition,offsetAndMetadata);
+            OffsetAndMetadata offsetAndMetadata = new OffsetAndMetadata(entry.getValue(), OffsetAndMetadata.NoMetadata(), -1);
+            Map<TopicAndPartition, OffsetAndMetadata> responseInfo = new HashMap<TopicAndPartition, OffsetAndMetadata>();
+            responseInfo.put(topicAndPartition, offsetAndMetadata);
             int correlationId = 0;
-            OffsetCommitRequest offsetCommitRequest = new OffsetCommitRequest(consumerGroup,responseInfo,correlationId,consumerGroup);
+            OffsetCommitRequest offsetCommitRequest = new OffsetCommitRequest(consumerGroup, responseInfo, correlationId, consumerGroup);
 
-            SimpleConsumer simpleConsumer = new SimpleConsumer(topicHost, topicPort, 100000, 64 * 1024,"OffsetCommitConsumer");
+            SimpleConsumer simpleConsumer = new SimpleConsumer(topicHost, topicPort, 100000, 64 * 1024, "OffsetCommitConsumer");
             OffsetCommitResponse offsetCommitResponse = simpleConsumer.commitOffsets(offsetCommitRequest);
 
 //            System.out.println(offsetCommitResponse.errorCode(topicAndPartition));
-        }
+         }
 
     }
+
+
+    private static Map<Integer, Long> getOldOffsets(String topic, String topicHost, Integer topicPort, String consumerGroup, Map<Integer, Long> partitionOffsets) {
+
+        Map<Integer, Long> oldPartitionOffsets = new HashMap<Integer, Long>();
+        for (Map.Entry<Integer, Long> entry : partitionOffsets.entrySet()) {
+
+            long oldOffset = FetchOffset.fetchOffset(topic,topicHost,topicPort,entry.getKey(),consumerGroup);
+            oldPartitionOffsets.put(entry.getKey(), oldOffset);
+        }
+
+
+        return oldPartitionOffsets;
+    }
+
 }
